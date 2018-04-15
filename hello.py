c550415e3cdd4fc5d47c6dab1c0ba5f3778b27e8
travis88 <hellsballs99@gmail.com>
2018-04-15 11:55:09 +0300

    initial commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+@app.route('/badrequest')
+def bad():
+    return '<h1>Bad request</h1>', 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
