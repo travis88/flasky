32e744a90c50a0ece96e9b88572dc99f2df4db21
travis88 <hellsballs99@gmail.com>
2018-04-15 15:31:06 +0300

    error pages added

--- hello.py
+++ hello.py
@@ -35,6 +35,14 @@ def my_redirect():
 def my_abort():
     abort(404)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     # app.run(debug=True)
     manager.run()
