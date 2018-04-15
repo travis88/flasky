86d65956e7aa66808ab1b2ec70037ac22f2b115d
travis88 <hellsballs99@gmail.com>
2018-04-15 12:32:28 +0300

    flask-script added

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, abort
+from flask_script import Manager
+
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -7,7 +10,7 @@ def index():
     return '<p>Your browser is %s</p>' % user_agent
 
 @app.route('/user/<name>')
-def user(name):
+def get_user(name):
     return '<h1>Hello, %s!</h1>' % name
 
 @app.route('/badrequest')
@@ -21,8 +24,13 @@ def cookie():
     return response
 
 @app.route('/redirect')
-def myredirect():
+def my_redirect():
     return redirect('http://example.com')
 
+@app.route('/abort')
+def my_abort():
+    abort(404)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    manager.run()
