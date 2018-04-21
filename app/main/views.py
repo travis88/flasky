f5d036f983bc94f076d8d8733a39fd65c9ecffe7
travis88 <hellsballs99@gmail.com>
2018-04-21 14:51:42 +0300

    authentication added

--- app/main/views.py
+++ app/main/views.py
@@ -3,8 +3,8 @@ from flask import render_template, session, redirect, url_for, current_app
 from . import main
 from .forms import NameForm
 from .. import db
-from .. models import User
-from .. email import send_mail
+from ..models import User
+from ..email import send_mail
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
