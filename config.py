6a7d67d6d1265ae6f873b2280d03595fa5cc5e2e
travis88 <hellsballs99@gmail.com>
2018-05-01 11:34:03 +0300

    notification admin about errors

--- config.py
+++ config.py
@@ -35,6 +35,29 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        # отправить администарторам письма с сообщениями об ошибках
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.FLASKY_MAIL_SENDER,
+            toaddrs=[cls.FLASKY_ADMIN],
+            subject=cls.FLASKY_MAIL_SUBJECT_PREFIX = ' Application Error',
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
