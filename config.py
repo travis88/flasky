2f1c73b71ef4d3d0b09767e1d3eaad6dc4707274
travis88 <hellsballs99@gmail.com>
2018-05-01 11:40:46 +0300

    logging in unix

--- config.py
+++ config.py
@@ -57,6 +57,17 @@ class ProductionConfig(Config):
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
+class UnixConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+        
+        # использование syslog для журналирования
+        import logging
+        from logging.handlers import SysLogHandler
+        syslog_handler = SysLogHandler()
+        syslog_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(syslog_handler) # /var/log/messages
 
 config = {
     'development': DevelopmentConfig,
